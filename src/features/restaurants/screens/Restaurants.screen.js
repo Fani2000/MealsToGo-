47bd0326b8801ca68bf1064914158babbd763c15
Fani <keorapetsefani2000@gmail.com>
2022-01-16 06:27:24 +0200

    Added flatList

--- src/features/restaurants/screens/Restaurants.screen.js
+++ src/features/restaurants/screens/Restaurants.screen.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo';
-import { Platform, SafeAreaView, StyleSheet } from 'react-native';
+import { FlatList, Platform, SafeAreaView, StyleSheet } from 'react-native';
 import SearchBar from '../../../components/SearchBar';
 import RestaurentInfo from '../components/Restaurants-info';
 import styled from 'styled-components/native';
@@ -12,11 +12,32 @@ const SafeAreaViewContainer = styled(SafeAreaView)`
   margin-top: ${isAndroid}px;
 `;
 
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const DATA = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 5 },
+  { id: 6 },
+  { id: 7 },
+];
+
 export default function RestaurantsScreen() {
   return (
     <SafeAreaViewContainer>
-      <SearchBar />
-      <RestaurentInfo />
+      <SearchContainer>
+        <SearchBar />
+      </SearchContainer>
+      <FlatList
+        data={DATA}
+        renderItem={() => <RestaurentInfo />}
+        keyExtractor={(item) => item.id}
+        contentContainerStyle={{ padding: 4 }}
+      />
     </SafeAreaViewContainer>
   );
 }
