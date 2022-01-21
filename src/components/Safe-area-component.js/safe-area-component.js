0965eafe2fdeff37d629aaa4b89a56f0cc855c51
Fani <keorapetsefani2000@gmail.com>
2022-01-21 03:12:09 +0200

    added ActivityIndicator from react-paper

--- src/components/Safe-area-component.js/safe-area-component.js
+++ src/components/Safe-area-component.js/safe-area-component.js
@@ -4,5 +4,6 @@ import styled from 'styled-components/native';
 const isAndroid = Platform.OS === 'android' ? StatusBar.currentHeight : 0;
 
 export const SafeAreaViewContainer = styled(SafeAreaView)`
+  flex: 1;
   margin-top: ${isAndroid}px;
 `;
--- src/features/restaurants/screens/Restaurants.screen.js
+++ src/features/restaurants/screens/Restaurants.screen.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, View } from 'react-native';
 import SearchBar from '../../../components/SearchBar';
 import RestaurentInfo from '../components/Restaurants-info';
 import styled from 'styled-components/native';
 import { SafeAreaViewContainer } from '../../../components/Safe-area-component.js/safe-area-component';
 import { RestaurantsContext } from '../../../services/restaurants/restaurant-context';
+import { ActivityIndicator, Colors } from 'react-native-paper';
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]} ${(props) => props.theme.space[2]};
@@ -19,12 +20,24 @@ export default function RestaurantsScreen() {
       <SearchContainer>
         <SearchBar />
       </SearchContainer>
-      <FlatList
-        data={restaurants}
-        renderItem={({ item }) => <RestaurentInfo restaurant={item} />}
-        keyExtractor={(item) => item.name}
-        contentContainerStyle={{ padding: 4 }}
-      />
+      {isLoading && (
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <ActivityIndicator
+            size={50}
+            style={{ marginLeft: -25 }}
+            color={Colors.red300}
+            animating={true}
+          />
+        </View>
+      )}
+      {!isLoading && (
+        <FlatList
+          data={restaurants}
+          renderItem={({ item }) => <RestaurentInfo restaurant={item} />}
+          keyExtractor={(item) => item.name}
+          contentContainerStyle={{ padding: 4 }}
+        />
+      )}
     </SafeAreaViewContainer>
   );
 }
