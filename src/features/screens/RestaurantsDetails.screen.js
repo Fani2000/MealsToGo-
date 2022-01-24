bee5de619d03151cab309e6a15586ba714c79db7
Fani <keorapetsefani2000@gmail.com>
2022-01-24 03:51:57 +0200

    added the List.Items in the RestaurantsDetails

--- src/features/screens/RestaurantsDetails.screen.js
+++ src/features/screens/RestaurantsDetails.screen.js
@@ -1,15 +1,59 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, ScrollView } from 'react-native';
+import React, { useState } from 'react';
 import { useRoute } from '@react-navigation/native';
 import RestaurentInfo from '../restaurants/components/Restaurants-info';
+import { List } from 'react-native-paper';
 
 const RestaurantsDetailsScreen = () => {
   const route = useRoute();
   const { restaurant } = route.params;
+  const [breakfastExpanded, setBreakFastExpanded] = useState(false);
+  const [lunchExpanded, setLunchExpanded] = useState(false);
+  const [dinnerExpanded, setDinnerExpanded] = useState(false);
+  const [drinksExpanded, setDrinksExpanded] = useState(false);
 
   return (
     <View>
       <RestaurentInfo restaurant={restaurant} />
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <List.Accordion
+          title="Breakfast"
+          left={(props) => <List.Icon {...props} icon="bread-slice" />}
+          onPress={() => setBreakFastExpanded(!breakfastExpanded)}
+        >
+          <List.Item title="Eggs Benedict" />
+          <List.Item title="Classic Breakfast" />
+        </List.Accordion>
+        <List.Accordion
+          title="Lunch"
+          left={(props) => <List.Icon {...props} icon="hamburger" />}
+          onPress={() => setLunchExpanded(!lunchExpanded)}
+        >
+          <List.Item title="Burger w/Fries" />
+          <List.Item title="Steak Sandwich" />
+          <List.Item title="Mushroom Soup" />
+        </List.Accordion>
+        <List.Accordion
+          title="Dinner"
+          left={(props) => <List.Icon {...props} icon="food-variant" />}
+          onPress={() => setDinnerExpanded(!dinnerExpanded)}
+        >
+          <List.Item title="Spaghetti Bolognese" />
+          <List.Item title="Beal Cutlet with Chicken Mushroom" />
+          <List.Item title="Steak Frites" />
+        </List.Accordion>
+        <List.Accordion
+          title="Drinks"
+          left={(props) => <List.Icon {...props} icon="cup" />}
+          onPress={() => setDrinksExpanded(!drinksExpanded)}
+        >
+          <List.Item title="Coffee" />
+          <List.Item title="Tea" />
+          <List.Item title="Modelo" />
+          <List.Item title="Coke" />
+          <List.Item title="Fanta" />
+        </List.Accordion>
+      </ScrollView>
     </View>
   );
 };
