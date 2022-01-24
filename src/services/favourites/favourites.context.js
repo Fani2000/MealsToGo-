a90716244a455f4967d22f9b9204ea30a716c226
Fani <keorapetsefani2000@gmail.com>
2022-01-24 23:04:30 +0200

    added the localstorage for the favourites

--- src/services/favourites/favourites.context.js
+++ src/services/favourites/favourites.context.js
@@ -1,9 +1,38 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const FavouritesContext = createContext();
 
+const saveFavourites = async (value) => {
+  try {
+    const jsonFavourites = JSON.stringify(value);
+    await AsyncStorage.setItem('@storage_favourites', jsonFavourites);
+  } catch (e) {
+    console.log('Error Storing', e);
+  }
+};
+
+const loadFavourites = async (value) => {
+  try {
+    const jsonFavourites = await AsyncStorage.getItem('@storage_favourites');
+    if (!jsonFavourites) {
+      setFavourites(JSON.parse(jsonFavourites));
+    }
+  } catch (e) {
+    console.log('Error loadng', e);
+  }
+};
+
 export const FavouriteContextProvider = (props) => {
   const [favourites, setFavourites] = useState([]);
+
+  useEffect(() => {
+    saveFavourites(favourites);
+  }, [favourites]);
+
+  useEffect(() => {
+    loadFavourites();
+  }, []);
 
   const add = (restaurant) => {
     setFavourites([...favourites, restaurant]);
